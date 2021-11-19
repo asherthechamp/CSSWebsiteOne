73e2242b8d1e1f2257b83cd7a9f5acd30947e0ba
Ashenafi <ashenafibordea@gmail.com>
2021-11-18 18:20:44 -0800

    First Post

---
+++ utilities.js
@@ -0,0 +1,32 @@
+date = (new Date("Mar 25 2015")).toDateString()
+
+window.onload = (e) => {
+    document.getElementById("created-date").innerHTML = date
+
+}
+
+function showMenu(){
+    navLinks = document.getElementById("navLinks")
+
+    navLinks.style.right = '0'
+}
+function hideMenu(){
+    navLinks = document.getElementById("navLinks")
+
+    navLinks.style.right = '-200px'
+}
+
+function showContent(ai, ac) {
+    articleIntro = document.getElementById(ai)
+    articleIntro.hidden = true
+
+    articleContent = document.getElementById(ac)
+    articleContent. hidden = false
+}
+
+function showComments(e, id) {
+    showCom = document.getElementById(id)
+    showCom.hidden = !(showCom.hidden)
+    e.text = e.text === "Hide Comments" ? "Show Comments" : "Hide Comments"
+
+}
