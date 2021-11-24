9f82365347ed7d629fe5ade1a856cf97fd532a18
Ashenafi <ashenafibordea@gmail.com>
2021-11-23 20:48:18 -0800

    csss

--- utilities.js
+++ utilities.js
@@ -1,32 +1,30 @@
-date = (new Date("Mar 25 2015")).toDateString()
+date = new Date("Mar 25 2015").toDateString();
 
 window.onload = (e) => {
-    document.getElementById("created-date").innerHTML = date
+  document.getElementById("created-date").innerHTML = date;
+};
 
+function showMenu() {
+  navLinks = document.getElementById("navLinks");
+
+  navLinks.style.right = "0";
 }
+function hideMenu() {
+  navLinks = document.getElementById("navLinks");
 
-function showMenu(){
-    navLinks = document.getElementById("navLinks")
-
-    navLinks.style.right = '0'
-}
-function hideMenu(){
-    navLinks = document.getElementById("navLinks")
-
-    navLinks.style.right = '-200px'
+  navLinks.style.right = "-500px";
 }
 
 function showContent(ai, ac) {
-    articleIntro = document.getElementById(ai)
-    articleIntro.hidden = true
+  articleIntro = document.getElementById(ai);
+  articleIntro.hidden = true;
 
-    articleContent = document.getElementById(ac)
-    articleContent. hidden = false
+  articleContent = document.getElementById(ac);
+  articleContent.hidden = false;
 }
 
 function showComments(e, id) {
-    showCom = document.getElementById(id)
-    showCom.hidden = !(showCom.hidden)
-    e.text = e.text === "Hide Comments" ? "Show Comments" : "Hide Comments"
-
+  showCom = document.getElementById(id);
+  showCom.hidden = !showCom.hidden;
+  e.text = e.text === "Hide Comments" ? "Show Comments" : "Hide Comments";
 }
